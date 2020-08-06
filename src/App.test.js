e1e461916f77fe4cb044192b6b56d333e89dcdc6
mfaisalpk <mfaisalpk@gmail.com>
2020-08-06 14:40:07 +0300

    tdd updated
    tdd updated

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello it is my first testing.../i);
+  const linkElement = getByText(/Hello it is my first TDD Test Deiven Development.../i);
   expect(linkElement).toBeInTheDocument();
 });
